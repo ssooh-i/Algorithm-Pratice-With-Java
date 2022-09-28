c52fd269b6e7a537249c2e5b7eed40bd36357fe1
ssooh-i <615ksh@gmail.com>
2022-09-28 22:30:07 +0900

    덱으로 고쳐서 풀어보셈

--- Algorithm/src/study/BOJ/g5_5430.java
+++ Algorithm/src/study/BOJ/g5_5430.java
@@ -2,9 +2,8 @@ package study.BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 //AC
@@ -18,13 +17,14 @@ public class g5_5430 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(st.nextToken());// 테스트 케이스 수
 		for (int tc = 1; tc <= T; tc++) {
 			char[] command = br.readLine().toCharArray();// 명령어 하나씩 잘라서 배열로 저장
 
 			int N = Integer.parseInt(br.readLine()); // 명령처리가 될 배열 길이
-			List<Integer> arr = new LinkedList<>();// 추가 될 곳
+			Deque<Integer> arr = new ArrayDeque<>();// 추가 될 곳
 
 			char[] a = br.readLine().toCharArray();
 
@@ -37,28 +37,48 @@ public class g5_5430 {
 				}
 			}
 
+			boolean flag = true;// 정방향
+
 			for (int i = 0; i < N; i++) {// 명령어 꺼내서 실행하기
 				for (int j = 0; j < command.length; j++) {
-					if (arr.isEmpty()) {// 배열이 비었으면 error 프린
-						System.out.println("error");
-					} else {
 
-						if (command[j] == 'R') { // 뒤집기
-							Collections.reverse(arr);
+					if (command[j] == 'R') { // 뒤집기
+						if (flag) {
+							flag = false;
+						} else {
+							flag = true;
 						}
-						if (command[j] == 'D') { // 버리기
-							arr.remove(0);// 0번째 삭제
+
+					}
+					if (command[j] == 'D') { // 버리기
+						if (arr.isEmpty()) {// 배열이 비었으면 error 프린
+							System.out.println("error");
+							break;
+						}else {
+						if (flag) {
+							arr.removeLast();// 0번째 삭제
+						} else {
+							arr.removeFirst();// 0번째 삭제
 						}
+						}
+						
 					}
 				}
 			}
-
-			for (int i : arr) {
-				System.out.print(i + " ");
+			
+			sb.append("[");
+			for (int aa = 0; aa < arr.size(); aa++) {
+				sb.append(arr.pollFirst());
+				if((arr.size())-1 == aa) {
+					continue;
+				}
+				sb.append(",");
 			}
-			System.out.println();
-
+			sb.append("]");
 		}
-	}
 
+		
+		System.out.println(sb.toString());
+
+	}
 }
