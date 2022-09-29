2263bac2e33d114c7735321bddc3e0261fa28590
ssooh-i <615ksh@gmail.com>
2022-09-29 23:46:34 +0900

    워크샵(백준 1463_1로 만들기) 숙제(백준 1149_RGB거리)

---
+++ Algorithm/src/homework/hw_0929/s1_1149.java
@@ -0,0 +1,36 @@
+package homework.hw_0929;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//RGB 거리
+public class s1_1149 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int[][] RGB = new int[N][3];
+		
+		for(int i =0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			RGB[i][0] = Integer.parseInt(st.nextToken()); //R
+			RGB[i][1] = Integer.parseInt(st.nextToken()); //G
+			RGB[i][2] = Integer.parseInt(st.nextToken()); //B
+		}
+		
+		//1부터 N-1까지, i-1의 RGB 중 최소값 찾아서 더하기
+		//0번째는 상관없음, 1이 판단하니까
+		for(int i = 1; i<N; i++) {
+			RGB[i][0] += Math.min(RGB[i-1][1], RGB[i-1][2]);//빨간색을 선택했으면 앞에 파랑이랑 초록을 봐줘야함
+			RGB[i][1] += Math.min(RGB[i-1][0], RGB[i-1][2]);
+			RGB[i][2] += Math.min(RGB[i-1][0], RGB[i-1][1]);
+		}
+		
+		//N-1의 RGB중에 젤 최소값 출력
+		System.out.println(Math.min(Math.min(RGB[N-1][0], RGB[N-1][1]),RGB[N-1][2]));
+		
+	}
+
+}
---
+++ Algorithm/src/workshop/ws0929/s3_1463.java
@@ -0,0 +1,27 @@
+package workshop.ws0929;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//1로 만들기
+public class s3_1463 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int X = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		while(X != 1) {
+			if(X == 1) {
+				System.out.println(cnt);
+				break;
+			}
+			
+			if(X%3 == 0) {}
+			else if(X%2 == 0) {}
+			else{
+				X =
+			}
+		}
+	}
+}
